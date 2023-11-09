39670674cc29ff51b146ba6cf7c72dd2f179122b
DoHyeong_Kim <maam10@naver.com>
2023-11-09 16:05:15 +0900

    Title: 연속되는 수 3, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

--- 231109/연속되는 수 3/continuous-number3.java
+++ 231109/연속되는 수 3/continuous-number3.java
@@ -2,28 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    private static String[] strArr = new String[1000];
-    private static int[] answer = new int[1002];
+    private static final int MAX_N = 1000;
+    private static int[] arr = new int[MAX_N];
+    private static int n;
+
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         for(int i = 0; i < n; i++){
-            String s = br.readLine();
-            strArr[i] = s;
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        int idx = 0;
 
+
+        int ans = 0, cnt = 0;
         for(int i = 0; i < n; i++){
-            answer[idx] ++;
-            if(i != 0 && (strArr[i].charAt(0)=='-') != (strArr[i-1].charAt(0) =='-')){
-                idx++;
+            if(i >= 1 && arr[i] * arr[i-1] > 0){
+                cnt++;
             }
-            else if(i != 0 && Character.isDigit(strArr[i].charAt(0)) != Character.isDigit(strArr[i-1].charAt(0))){
-                idx++;
+            else{
+                cnt = 1;
             }
+            ans = Math.max(ans,cnt);
         }
-        Arrays.sort(answer);
-        System.out.print(answer[answer.length-1]);
+        System.out.print(ans);
     }
 }
