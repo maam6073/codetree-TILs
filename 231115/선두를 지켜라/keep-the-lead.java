dc69d1c9142dd4998014826dcb1a705fcebcf897
DoHyeong_Kim <maam10@naver.com>
2023-11-15 13:47:26 +0900

    Title: 선두를 지켜라, Time: 85ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 231115/선두를 지켜라/keep-the-lead.java
+++ 231115/선두를 지켜라/keep-the-lead.java
@@ -7,7 +7,6 @@ public class Main {
     static int m;
     static int[] a = new int[MAX_N+1];
     static int[] b = new int[MAX_N+1];
-    static int resultCnt = 0;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,30 +14,46 @@ public class Main {
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
 
-        int timeA = 1 , timeB = 1 , MAX_V = 0;
-        for(int i = 0; i < n+m; i++){
+        int timeA = 1;
+        for(int i = 0; i < n; i++){
             s = br.readLine().split(" ");
             int v = Integer.parseInt(s[0]);
             int t = Integer.parseInt(s[1]);
-            if(i % 2 == 0){
-                while(t-- > 0){
-                    a[timeA] += v;
-                    timeA++;
-                }
-            }
-            else{
-                while(t-- > 0){
-                    a[timeB] += v;
-                    timeB++;
-                }
-            }
-            int temp = MAX_V;
-            MAX_V = Math.max(a[timeA],b[timeB]);
-            if(temp != MAX_V){
-                resultCnt++;
+            while(t-- > 0){
+                a[timeA] += v;
+                timeA++;
             }
         }
 
-        System.out.print(resultCnt);
+        int timeB = 1;
+        for(int i = 0; i < m; i++){
+            s = br.readLine().split(" ");
+            int v = Integer.parseInt(s[0]);
+            int t = Integer.parseInt(s[1]);
+            while(t-- > 0){
+                b[timeB] += v;
+                timeB++;
+            }
+        }
+        
+
+        int leader = 0, ans = 0;
+        for(int i = 1; i < timeA; i++){
+            if(a[i] > b[i]){
+                if(leader == 2){
+                    ans ++;
+                }
+
+                leader = 1;
+            }
+            else if(a[i] < b[i]){
+                if(leader == 1)
+                    ans++;
+
+                leader = 2;
+            }
+        }
+
+        System.out.print(ans);
     }
 }
