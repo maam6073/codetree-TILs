17aff4de1ec9ba2dc65a1b645ba4feb73f7e7c9e
DoHyeong_Kim <maam10@naver.com>
2023-11-22 22:01:30 +0900

    Title: 되돌아오기 2, Time: 130ms, Memory: 11MB, Status: Passed - CodeTree

--- 231122/되돌아오기 2/come-back-2.java
+++ 231122/되돌아오기 2/come-back-2.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Main {
     private static int n;
-    private static int[][] answer;
     private static int[] dx = new int[]{0,1,0,-1};
     private static int[] dy = new int[]{1,0,-1,0};
     private static int dirNum = 3;
@@ -14,17 +13,17 @@ public class Main {
     private static boolean inRange(int x,int y){
         return x >= 0 && x < n && y >= 0 && y < n;
     }
+    
 
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         n = s.length();
-        answer = new int[n][n];
         sP = (n/2) -1;
         x = sP;
         y = sP;
-
+        int time = 0;
     
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == 'F'){
@@ -41,10 +40,10 @@ public class Main {
             }
 
             if(inRange(x,y))
-                answer[x][y] = i+1;
+                time = i+1;
             
             if(i != 0 && x == sP && y == sP){
-                result = answer[x][y];
+                result = time;
                 break;
             }
         }
