e263342888e4b06f8900b1d16fbc54462d3b9cd3
DoHyeong_Kim <maam10@naver.com>
2023-11-22 22:06:10 +0900

    Title: 되돌아오기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 231122/되돌아오기/come-back.java
+++ 231122/되돌아오기/come-back.java
@@ -1,70 +1,68 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    public static final int DIR_NUM = 4;
-    
-    public static int n, x, y;
-    
-    // 동, 서, 남, 북 순으로 dx, dy를 정의합니다.
-    public static int[] dx = new int[]{1, -1,  0, 0};
-    public static int[] dy = new int[]{0,  0, -1, 1};
-    
-    // 답을 저장합니다.
-    public static int ans = -1;
-    
-    // 지금까지 걸린 시간을 기록합니다.
-    public static int elapsedTime;
-    
-    // dir 방향으로 dist 만큼 이동하는 함수입니다.
-    // 만약 시작지에 도달하면 true를 반환합니다.
-    public static boolean move(int dir, int dist) {
-        while(dist-- > 0) {
-            x += dx[dir];
-            y += dy[dir];
-            
-            // 이동한 시간을 기록합니다.
-            elapsedTime++;
-    
-            // 시작지로 다시 돌아오면,
-            // 답을 갱신해줍니다.
-            if(x == 0 && y == 0) {
-                ans = elapsedTime;
-                return true;
-            }
-        }
-        
-        return false;
+    private static int MAX_N = 100;
+    private static int n;
+    private static int[][] arr = new int[MAX_N][MAX_N];
+    private static int[] dx = new int[]{0, 1, 0, -1};
+    private static int[] dy = new int[]{1, 0, -1, 0};
+    private static int dir = 0;
+    private static int sP = 49;
+    private static int x = sP, y = sP;
+
+    private static void dirNumChange(char d){
+        if(d == 'N')
+            dir = 3;
+        else if(d == 'W')
+            dir = 2;
+        else if(d == 'S')
+            dir = 1;
+        else if(d == 'E')
+            dir = 0;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력
-        n = sc.nextInt();
-        
-        // 움직이는 것을 진행합니다.
-        while(n-- > 0) {
-            char cDir = sc.next().charAt(0);
-            int dist = sc.nextInt();
-            
-            // 각 방향에 맞는 번호를 붙여줍니다.
-            int dir;
-            if(cDir == 'E')
-                dir = 0;
-            else if(cDir == 'W')
-                dir = 1;
-            else if(cDir == 'S')
-                dir = 2;
-            else
-                dir = 3;
-            
-            // 주어진 방향대로 dist 만큼 위치를 이동해봅니다.
-            boolean done = move(dir, dist);
-            
-            // 시작 위치에 도달했다면, 종료합니다.
-            if(done)
+    private static boolean inRange(int x,int y){
+        return x >= 0 && x < MAX_N && y >= 0 && y < MAX_N;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        boolean check = false;
+
+        for(int i = 0; i < n; i++){
+            String[] s = br.readLine().split(" ");
+            char d = s[0].charAt(0);
+            int distance = Integer.parseInt(s[1]);
+            dirNumChange(d);
+
+            for(int j = 0; j < distance; j++){
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(inRange(nx,ny))
+                    arr[nx][ny] = cnt;
+
+                if(i != 0 && ny == sP && nx == sP){
+                    check = true;
+                    break;
+                }
+
+                cnt++;
+
+                x = nx;
+                y = ny;
+            }
+
+            if(check)
                 break;
+            else if(i == n-1 && !check)
+                cnt = -1;
         }
-        
-        System.out.print(ans);
+
+        System.out.print(cnt);
     }
 }
