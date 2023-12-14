70a8911fbbb6813158afa7a172910f8017bfa145
DoHyeong_Kim <maam10@naver.com>
2023-12-14 19:50:40 +0900

    Title: 이상한 진수 2, Time: 137ms, Memory: 8MB, Status: Passed - CodeTree

--- 231214/이상한 진수 2/awkward-digits-2.java
+++ 231214/이상한 진수 2/awkward-digits-2.java
@@ -10,18 +10,42 @@ public class Main {
         b = a.clone();
         int max = 0;
 
-        for(int i = 0; i < a.length; i++){
-            int result = 0;
-            int cnt = 0;
-            if(a[i].equals("0"))
-                a[i] = "1";
-
-            for(int j = a.length-1; j >= 0; j--){
-                result += Integer.parseInt(a[j]) * Math.pow(2,cnt++);
+        boolean check = true;
+        for (String s : a) {
+            if (s.equals("0")) {
+                check = false;
+                break;
             }
-            max = Math.max(result,max);
-            a[i] = b[i];
         }
+        if(!check) {
+            for (int i = 0; i < a.length; i++) {
+                int result = 0;
+                int cnt = 0;
+                if (a[i].equals("0"))
+                    a[i] = "1";
+
+                for (int j = a.length - 1; j >= 0; j--) {
+                    result += Integer.parseInt(a[j]) * Math.pow(2, cnt++);
+                }
+                max = Math.max(result, max);
+                a[i] = b[i];
+            }
+        }
+        else {
+            for (int i = 0; i < a.length; i++) {
+                int result = 0;
+                int cnt = 0;
+                if (a[i].equals("1"))
+                    a[i] = "0";
+
+                for (int j = a.length - 1; j >= 0; j--) {
+                    result += Integer.parseInt(a[j]) * Math.pow(2, cnt++);
+                }
+                max = Math.max(result, max);
+                a[i] = b[i];
+            }
+        }
+
 
         if(a.length <= 1)
             System.out.print(0);
