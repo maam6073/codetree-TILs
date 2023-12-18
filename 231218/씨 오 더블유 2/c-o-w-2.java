5be4b9193b0b31609eebacbb053d1c6d88ee015c
DoHyeong_Kim <maam10@naver.com>
2023-12-18 14:29:16 +0900

    Title: 씨 오 더블유 2, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 231218/씨 오 더블유 2/c-o-w-2.java
+++ 231218/씨 오 더블유 2/c-o-w-2.java
@@ -10,11 +10,16 @@ public class Main {
 
         int cnt = 0;
         for(int i = 0; i < n; i++){
+            if(s[i].equals("C")){
             for(int j = i +1; j < n; j++){
+                if(s[j].equals("O")){
                 for(int k = j +1; k < n; k++){
-                    if(!s[i].equals(s[j]) && !s[j].equals(s[k]))
+                    if(s[k].equals("W")){
                         cnt++;
+                    }
                 }
+                }
+            }
             }
         }
         
